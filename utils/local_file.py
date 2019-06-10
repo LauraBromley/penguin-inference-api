5537a4b0f6c21ed16afcbd5bdcb43227e240c428
Laura Bromley <laura@silvermetric.net>
2019-06-10 13:12:21 +0100

    added jupyter notebooks to repo

--- utils/local_file.py
+++ utils/local_file.py
@@ -3,7 +3,7 @@ from io import BytesIO
 
 # Load model from tmp folder, used for testing
 def setup_inf_model():
-    model_path = "D:\\DeepLearning\\website\\dl-model\\export.pkl"
+    model_path = "D:\\DeepLearning\\fastai-course\\Experiments\\exports\\squish\\export.pkl"
     with open(model_path, mode='rb') as file:
         fileContent = file.read()
     return init_model(BytesIO(fileContent))
